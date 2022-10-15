d42bb24495e500bf2744c3cc009e3d1e3bc019d9
Seth T <seth10@users.noreply.github.com>
2022-10-15 01:20:53 -0700

    Add a Lambda function to CDK
    This includes the code the lambda runs, some test events (added to git, but I can't currently make a Construct of these so you'll have to copy-paste to use them), and the CDK code to create a Lambda using this code. I also configure some specifics of the Lambda.

--- lib/serverless-minecraft-server-stack.ts
+++ lib/serverless-minecraft-server-stack.ts
@@ -1,19 +1,28 @@
 import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 
 export class ServerlessMinecraftServerStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'ServerlessMinecraftServerQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const startServer = new lambda.Function(this, 'Start_Minecraft_Server', {
+      runtime: lambda.Runtime.PYTHON_3_8,
+      code: lambda.Code.fromAsset('lambda/Start_Minecraft_Server'),
+      handler: 'lambda_function.lambda_handler',
+      timeout: Duration.minutes(5),
+      layers: [], /* TODO: add mcipi layer */
+      environment: {
+        API_GATEWAY_WEBSOCKET: 'https://7rlcxgf5oa.execute-api.us-west-2.amazonaws.com/production',
+        CONNECTIONS_TABLE_NAME: 'Minecraft_WebSocket_Connections',
+        INSTANCE_ID: 'i-51038532bec4d2196',
+        RCON_PASSWORD: 'G5Q0HMMGoKj0DSwQlParFeNJ25jds8rg',
+        WORLDS_TABLE_NAME: 'Minecraft_Worlds'
+      },
+      // TODO: define role
     });
 
-    const topic = new sns.Topic(this, 'ServerlessMinecraftServerTopic');
+    // TODO: setup triggers from API Gateway
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
